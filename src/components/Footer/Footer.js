8dea08794ec4baaa479f908fc5d02918c6c58d6d
Benjamin Cha <mrbrenjamincha@gmail.com>
2024-08-30 18:21:10 -0600

    Update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,12 +2,15 @@ import './Footer.css'
 
 const Footer = () => (
   <footer className='footer'>
+    <span>Created by </span>
     <a
       href='https://github.com/rjshkhr/cleanfolio'
       className='link footer__link'
     >
-      Created By Raj Shekhar
+      Raj Shekhar
     </a>
+    <span>, customized by </span>
+    <span className="footer-name">Ben Cha</span>
   </footer>
 )
 
