8fedf24ab12938b840dd5dc378f45f3e4b721d8c
Benjamin Cha <mrbrenjamincha@gmail.com>
2024-09-05 13:00:46 -0600

    Clean up code

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,8 @@
-import GitHubIcon from '@material-ui/icons/GitHub'
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import { about } from '../../portfolio'
 import './About.css'
 
 const About = () => {
-  const { name, role, description, resume, social } = about
+  const { name, role, description } = about
 
   return (
     <div className='about center'>
@@ -16,40 +14,7 @@ const About = () => {
 
       {role && <h2 className='about__role'>{role}</h2>}
       <p className='about__desc'>{description && description}</p>
-
-      {/* <div className='about__contact center'>
-        {resume && (
-          <a href={resume}>
-            <span type='button' className='btn btn--outline'>
-              Resume
-            </span>
-          </a>
-        )}
-
-        {social && (
-          <>
-            {social.github && (
-              <a
-                href={social.github}
-                aria-label='github'
-                className='link link--icon'
-              >
-                <GitHubIcon />
-              </a>
-            )}
-
-            {social.linkedin && (
-              <a
-                href={social.linkedin}
-                aria-label='linkedin'
-                className='link link--icon'
-              >
-                <LinkedInIcon />
-              </a>
-            )}
-          </>
-        )}
-      </div> */}
+      
     </div>
   )
 }
